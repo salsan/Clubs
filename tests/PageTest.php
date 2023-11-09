7ed347fe1d800e64fb657cb7f42b8c7e07973e77
salsan <me@salsan.dev>
2023-11-09 16:05:18 +0100

    chore: added new test unit

---
+++ tests/PageTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+use Salsan\Clubs;
+
+final class PageTest extends TestCase
+{
+    private $paramters = array(
+        'clubId' => '4058',
+        'year' => '2004',
+    );
+
+    public function testInit(): object
+    {
+        $clubs = new Clubs\Query($this->paramters);
+        $this->assertIsObject($clubs);
+
+        return $clubs;
+    }
+
+    /**
+     * @depends testInit
+     */
+    public function testGetInfo($clubs): void
+    {
+        $info = $clubs->getInfo();
+
+        foreach ($info as $club) {
+            $this->assertStringContainsStringIgnoringCase('A.C.S. SCACCHI HERA', $club['name']);
+            $this->assertStringContainsStringIgnoringCase('SA', $club['province']);
+            $this->assertStringContainsStringIgnoringCase('CAMPANIA', $club['region']);
+            $this->assertIsString($club['president']);
+            $this->assertIsString($club['website']);
+            $this->assertIsNumeric($club['address']['postal_code']);
+            $this->assertIsString($club['address']['street']);
+            $this->assertStringContainsStringIgnoringCase('PAESTUM' , $club['address']['city']);
+            $this->assertEquals(2, count($club['contact']));
+            $this->assertGreaterThanOrEqual(0, count($club['councilors']));
+        }
+    }
+
+    /**
+     * @depends testInit
+     */
+    public function testGetNumber($clubs): void
+    {
+        $this->assertEquals($clubs->getNumber(), '1');
+    }
+}
