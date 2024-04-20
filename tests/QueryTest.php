cac092ceb995320ef275227bcf3ade559961f5df
Salvatore Santagati <me@salsan.dev>
2024-04-20 20:07:08 +0200

    feat: improved test

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -31,14 +31,14 @@ final class QueryClubTest extends TestCase
             $this->assertStringContainsStringIgnoringCase('ASD ACCADEMIA SCACCHISTICA DON PIETRO CARRERA', $club['name']);
             $this->assertStringContainsStringIgnoringCase('Catania', $club['province']);
             $this->assertStringContainsStringIgnoringCase('SICILIA', $club['region']);
-            $this->assertStringContainsStringIgnoringCase('3517674477', $club['contact']['tel'] );
+            $this->assertStringContainsStringIgnoringCase('3517674477', $club['contact']['tel']);
             $this->assertStringContainsStringIgnoringCase('accadpcarrera@email.it', $club['contact']['email']);
             $this->assertStringContainsStringIgnoringCase('LEONARDI DANIELE', $club['president']);
             $this->assertContains('SANTAGATI ALESSANDRO', $club['councilors']);
-            $this->assertIsString($club['website']);
-            // $this->assertIsNumeric($club['address']['postal_code']);
-            $this->assertIsString($club['address']['street']);
-            $this->assertStringContainsStringIgnoringCase('Catania' , $club['address']['city']);
+            $this->assertStringContainsStringIgnoringCase('https://www.accademiacarrera.it', $club['website']);
+            $this->assertStringContainsStringIgnoringCase($club['address']['postal_code'], '95100');
+            $this->assertStringContainsStringIgnoringCase('VIA CASA NUTRIZIONE, 38', $club['address']['street']);
+            $this->assertStringContainsStringIgnoringCase('Catania', $club['address']['city']);
             $this->assertEquals(2, count($club['contact']));
             $this->assertGreaterThanOrEqual(3, count($club['councilors']));
         }
